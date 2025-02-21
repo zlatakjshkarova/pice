90cd53015e6bbded3faf22c609780b557a6e6f90
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2025-02-22 00:08:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let price = document.querySelector('#price');
+let s = document.querySelector('#S');
+let consumer = document.querySelector('#consumer');
+let cost = document.querySelector('#cost"');
+let btn = document.querySelector('#btn')
